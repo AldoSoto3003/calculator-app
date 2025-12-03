b7e983ddaed8fcd86c0aa3a7d351a805e7432b77
Aldo Soto <asoto@ince.con.mx>
2025-12-03 09:53:16 -0800

    Basic arithmetics functions

--- hooks/useCalculator.tsx
+++ hooks/useCalculator.tsx
@@ -22,6 +22,28 @@ export const useCalculator = () => {
     const buildNumber = ( numberString: string ) => {
         // Check if decimal point alredy exists
         if (number.includes('.') && numberString === '.') return;
+
+        if (number.startsWith('0') || number.startsWith('-0')) {
+            if (numberString === '.'){
+                return setNumber(number + numberString);
+            } 
+            // Evaluate if the current number is zero and de the actual number includes a decimal point
+            if (numberString === '0' && number.includes('.')) {
+                return setNumber(number + numberString);
+            }
+    
+            // Evaluate if the number is different of zero , there is not a decimal point AND it is the first number, 
+            // then avoid to write something like '09' instead '9'
+            if (numberString !== '0' && !number.includes('.')) {
+                return setNumber(numberString);
+            }
+    
+            // Avoid 00000.000
+            if (numberString === '0' && !number.includes('.')){
+                return;
+            }
+        }
+        setNumber(number + numberString)
     }
 
     return {
